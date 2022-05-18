30c93e3b953ab0e133d5e4fa04ad2ca9fc21c79a
mafer0027 <mbarahona@zventus.com>
2022-05-18 10:35:39 -0600

    asas

--- src/views/poc/dataTable.js
+++ src/views/poc/dataTable.js
@@ -6,10 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const DataTable = () => {
   const columns = useMemo(() => [
     {
-      Header: 'Product',
-      accesor: 'product',
-    },
-    {
       Header: 'Date',
       accesor: 'date',
     },
